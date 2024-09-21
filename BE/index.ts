d73c45ae89c7f1b84128427cc8d7011eccd87f55
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-09-21 14:32:28 +0300

    update Be

--- BE/index.ts
+++ BE/index.ts
@@ -8,10 +8,10 @@ import { connectDB } from '#db/connect'
 import { Logger } from '#helpers/logger'
 import { router as eventRouter } from '#routes/event.route'
 
-const __dirname = path.resolve()
 dotenv.config()
 
 const app = express()
+const __dirname = path.resolve()
 
 app.use(express.json())
 app.use(cors({ origin: '*', credentials: true }))
@@ -23,15 +23,6 @@ app.get('/health', (_: Request, res: Response) => res.status(200).json({ success
 const prefix = '/api'
 app.use(`${prefix}/events`, eventRouter)
 
-app.all('*', (_: Request, res: Response) => res.status(404).json({ success: false, msg: 'Page not found' }))
-
-/** Start Server **/
-const PORT = process.env.PORT ? Number(process.env.PORT) : 3000
-
-http.createServer(app).listen(PORT, () => {
-    connectDB()
-    Logger.info(`Server is listening on port ${PORT}`)
-})
 /** Static Server **/
 if (process.env.NODE_ENV === 'production') {
     app.use(
@@ -50,3 +41,13 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.join(__dirname, '/FE', 'dist', 'index.html'))
     })
 }
+
+app.all('*', (_: Request, res: Response) => res.status(404).json({ success: false, msg: 'Page not found' }))
+
+/** Start Server **/
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000
+
+http.createServer(app).listen(PORT, () => {
+    connectDB()
+    Logger.info(`Server is listening on port ${PORT}`)
+})
